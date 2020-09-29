ffb4bf6cbc056912be493c6b3bdb9f54e7ddfe25
RyanLauret <66676905+RyanLauret@users.noreply.github.com>
2020-09-29 22:36:01 +0400

    Create charte.php

---
+++ charte.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+	<head>
+		<title>DRS sports</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="charte.css">
+		<link rel="stylesheet" type="text/css" href="barrenav.css"/>
+</head>
+
+	<body>
+		<!--Couleur du titre et création de la classe "title"-->
+		<div class= "title">DRS Sport</div>
+
+		<!--Création de la classe "button"-->
+		<?php 
+			include("barrenav.php");
+		?>
+
+ 		
+ 		  	
+		 		<div class="charte">
+		 			
+
+
+		 			
+		 					<img src="imageweb/index.png"
+		 					alt="imageweb"
+						style=float:right
+						>
+
+		 			
+					<h1>Charte de qualité</h1>
+
+		 			
+
+		 		<p>Tous les concessionaires et garage de DRS Sport se sont engagés à respecter et à être évalués sur notre charte de qualité qui repose sur les points suivants:</p>
+
+
+		 		<h3 style="text-decoration: underline;">Respect horaire de restitution</h3>
+		 		<p>Le concessionaire de DRS Sport s’engage à respecter l’horaire de restitution indiqué au client  lors de la remise de son véhicule. En cas d’évènement exceptionnel, le concessionaire s’engage à prévenir au plus vite le client pour trouver la meilleure solution.</p>
+
+
+
+		 	<h3 style="text-decoration: underline;">Accueil personnalisé</h3>		 		
+		 		<p>Le garage et le concessionaire de DRS Sport s'engage à accueillir personnellement ses clients, à être à leur l'écoute </p>
+
+		 		
+		 		<h3 style="text-decoration: underline;">Qualité intervention</h3>
+		 		<p>Le garage de DRS Sport s’engage à réaliser l’intervention dans les règles de l’art et en suivant les préconisations du constructeur du véhicule. Nos garages sont tous équipés et formés pour intervenir sur les véhicules qu’ils reçoivent.</p>
+
+
+		 		<h3 style="text-decoration: underline;">Environnements</h3>
+		 		<p>Le concessionaire DRS Sport s’engage a avoir de plus en plus de voiture electrique ou hybride dans les prochaines années</p>
+		 		
+
+
+
+
+
+
+
+
+
+
+
+ 		  		
+
+		 			</div>
+
+	 			
+		</body>
+</html>
