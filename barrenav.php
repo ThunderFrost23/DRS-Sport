41a65cf91ccaa154ea11d8d0506284d0a63749b7
RyanLauret <66676905+RyanLauret@users.noreply.github.com>
2020-09-29 22:34:13 +0400

    Create barrenav.php
    Navbar of the website

---
+++ barrenav.php
@@ -0,0 +1,10 @@
+<!--Création de la classe "button"-->
+<nav class="menu">
+		<ul class="menu__list">
+	    <li class="menu__group"><a href="index.php" class="menu__link">Accueil</a></li>
+	    <li class="menu__group"><a href="galerie.php" class="menu__link">Galerie</a></li>
+	    <li class="menu__group"><a href="serviceetaccessoires.php" class="menu__link">Services Proposés</a></li>
+	    <li class="menu__group"><a href="charte.php" class="menu__link">Charte</a></li>
+	    <li class="menu__group"><a href="contact.php" class="menu__link">Contact</a></li>
+	</ul>
+</nav>
