efc7f990f7b0922d77cc070e60b7d693dc6ec078
RyanLauret <66676905+RyanLauret@users.noreply.github.com>
2020-09-29 22:40:53 +0400

    Create serviceetaccessoires.php

---
+++ serviceetaccessoires.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+	<head>
+		<!--Titre-->
+		<title>DRS Sport-Galerie</title>
+		<meta charset="UTF-8">
+		<!--Lien vers le CSS-->
+		<link rel="stylesheet" type="text/css" href="service.css"/>
+		<link rel="stylesheet" type="text/css" href="barrenav.css"/>
+	</head>	
+
+	<body>
+		<!--Couleur du titre et création de la classe "title"-->
+		<div class= "title">DRS Sport</div>
+
+<!--Création de la classe "button"-->
+		<?php 
+			include("barrenav.php");
+		?>
+
+			 	
+s 		  	<div>
+ 		  		
+
+		 			<h2>Les Ateliers Après-Vente</h2>
+
+		 		<div class="service">
+			 		
+		 			<img  src="imageweb/atelier_1.jpg"
+		 				alt="atelier"
+						style="float:right"
+						>
+
+
+			 		<h3>-Services proposés: </h3>
+
+			 		
+
+					<p> <b>-Entretien :</b> essentiel au bon fonctionnement de votre véhicule, l'entretien régulier favorise une consommation modérée de carburant et assure la longévité du moteur. </p>
+					<p>   </p>
+					<p><b>-Pneumatiques :</b> seul lien avec la route, des pneumatiques en bon état réduisent la consommation de carburant, et garantissent une puissance de freinage optimale en cas d'urgence.</p>
+					<p>   </p>
+					<p><b>-Réparations :</b> DRS Sport utilise exclusivement des pièces de rechange d'origine, taillées sur mesure pour votre véhicule.</p>
+					<p>
+
+
+						
+				</div>
+
+				<br>
+
+				<h2>Boutique accessoires</h2>
+
+
+				<div class="contenueAccessoire">
+
+					<h3>Alfa Romeo Giulia Quadrifoglio</h3>
+
+					<div class="encadrementAcc">
+						<img alt="Alfa" src="imageweb/AlfaRomeoGiuliaQuadrifoglio/jante1.png" height="140" width="140"> 
+
+						<div class="titre"> JANTES en alliage 19" 'Quadrifoglio'noir </div> <div class="prix"> 1200€ </div>
+						<a href="imageweb/AlfaRomeoGiuliaQuadrifoglio/jante1.png"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="Alfa" src="imageweb/AlfaRomeoGiuliaQuadrifoglio/jante2.png" height="140" width="140"> 
+
+						<div class="titre"> JANTES en alliage 19" 'Lightweight'</div> <div class="prix"> 1200€ </div>
+						<a href="imageweb/AlfaRomeoGiuliaQuadrifoglio/jante2.png"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="Alfa" src="imageweb/AlfaRomeoGiuliaQuadrifoglio/jante3.png" height="140" width="140"> 
+
+						<div class="titre"> JANTES en alliage 19" 'Lightweight' ,noir</div> <div class="prix"> 1200€ </div>
+						<a href="imageweb/AlfaRomeoGiuliaQuadrifoglio/jante3.png "> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="Alfa" src="imageweb/AlfaRomeoGiuliaQuadrifoglio/retro1.jpg" height="130" width="150"> 
+
+						<div class="titre"> Coque de rétroviseurs extérieurs</div> <div class="prix">  500€ </div>
+						<a href="imageweb/AlfaRomeoGiuliaQuadrifoglio/retro1.jpg" > <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="Alfa" src="imageweb/AlfaRomeoGiuliaQuadrifoglio/siege1.jpg" height="130" width="150"> 
+
+						<div class="titre"> Siège baquet EVO</div> <div class="prix">  800€  </div>
+						<a href="imageweb/AlfaRomeoGiuliaQuadrifoglio/siege1.jpg"  > <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+
+				</div>
+
+
+
+				<br>
+
+
+
+				<div class="contenueAccessoire">
+
+					<h3>AudiR8V10PlusSelection24h</h3>
+
+					<div class="encadrementAcc">
+						<img alt="Audi" src="imageweb/AudiR8V10PlusSelection24h/jante1.png" height="140" width="140"> 
+
+						<div class="titre"> Jantes en alliage léger 19", 5 bras doubles </div> <div class="prix"> 1300€ </div>
+						<a href="imageweb/AudiR8V10PlusSelection24h/jante1.png"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="Audi" src="imageweb/AudiR8V10PlusSelection24h/jante2.png" height="140" width="140"> 
+
+						<div class="titre"> jantes en alliage léger 20", 10 bras noir en Y , noir</div> <div class="prix"> 1700€ </div>
+						<a href="imageweb/AudiR8V10PlusSelection24h/jante2.png"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					</div>
+
+
+					<br>
+
+
+
+					<div class="contenueAccessoire">
+
+					<h3>BMW I8</h3>
+
+					<div class="encadrementAcc">
+						<img alt="BMW" src="imageweb/BMWI8/jante1.jpg" height="140" width="140"> 
+
+						<div class="titre"> Jantes en alliage leger profilees 20" style 444 Turbine</div> <div class="prix"> 1000€ </div>
+						<a href="imageweb/BMWI8/jante1.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="BMW" src="imageweb/BMWI8/jante2.jpg" height="140" width="140"> 
+
+						<div class="titre">antes en alliage leger 20" style 516 Black a rayons radiaux</div> <div class="prix"> 1500€ </div>
+						<a href="imageweb/BMWI8/jante2.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="BMW" src="imageweb/BMWI8/jante3.jpg" height="140" width="140"> 
+
+						<div class="titre">Jantes en alliage leger profilees 20" style 625  Turbine</div> <div class="prix"> 1800€ </div>
+						<a href="imageweb/BMWI8/jante3.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="BMW" src="imageweb/BMWI8/interieur1.jpg" height="140" width="160"> 
+
+						<div class="titre"> Cuir naturel exclusif noir Amido avec tissu bleu</div> <div class="prix"> 800€ </div>
+						<a href="imageweb/BMWI8/interieur1.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+						<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="BMW" src="imageweb/BMWI8/interieur2.jpg" height="140" width="160"> 
+
+						<div class="titre"> Cuir naturel exclusif noir Amido avec tissu E-Copper</div> <div class="prix"> 800€ </div>
+						<a href="imageweb/BMWI8/interieur2.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+					</div>
+
+					<br>	
+
+
+					<div class="contenueAccessoire">
+
+					<h3>Mercedes Classe A 45 AMG</h3>
+
+					<div class="encadrementAcc">
+						<img alt="Mercedes" src="imageweb/MercedesClasseA45AMG/jante1.jpg"  width="180"> 
+
+						<div class="titre"> JANTES AMG à 5 double bramche </div> <div class="prix"> 1300€ </div>
+						<a href="imageweb/MercedesClasseA45AMG/jante1.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="Mercedes" src="imageweb/MercedesClasseA45AMG/jante2.jpg"  width="180"> 
+
+						<div class="titre"> JANTES multibranches AMG</div> <div class="prix"> 1300€ </div>
+						<a href="imageweb/MercedesClasseA45AMG/jante2.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					</div>
+
+					<br>
+
+
+
+					<div class="contenueAccessoire">
+
+					<h3>Peugeot 208 gtline</h3>
+
+					<div class="encadrementAcc">
+						<img alt="Peugeot" src="imageweb/Peugeot208gtline/jante1.jpg" height="140" width="140"> 
+
+						<div class="titre">Japan Racing JR-21 17x7 ET25 Noir Mat </div> <div class="prix"> 160€ </div>
+						<a href="imageweb/Peugeot208gtline/jante1.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="Peugeot" src="imageweb/Peugeot208gtline/jante2.jpg" height="140" width="140"> 
+
+						<div class="titre">Japan Racing JR-3 Noir mat 17x8 ET25 </div> <div class="prix"> 200€ </div>
+						<a href="imageweb/Peugeot208gtline/jante2.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<div class="encadrementAcc">
+						<img alt="Peugeot" src="imageweb/Peugeot208gtline/jante3.jpg" height="140" width="140"> 
+
+						<div class="titre"> SUPERTURISMO LM 17 pouces  </div> <div class="prix"> 300€ </div>
+						<a href="imageweb/Peugeot208gtline/jante3.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="Peugeot" src="imageweb/Peugeot208gtline/siege1.jpg" height="140" width="150"> 
+
+						<div class="titre">sièges baquet Peugeot 208 GT line by Peugeot Sport </div> <div class="prix"> 200€ </div>
+						<a href="imageweb/Peugeot208gtline/siege1.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="Peugeot" src="imageweb/Peugeot208gtline/spoiler1.jpg" height="150" width="150"> 
+
+						<div class="titre">Rajout de pare chocs avant Peugeot 208</div> <div class="prix"> 1300€ </div>
+						<a href="imageweb/Peugeot208gtline/spoiler1.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+					</div>
+
+
+					<br>
+
+
+
+				<div class="contenueAccessoire">
+
+					<h3>Renault Megane RS</h3>
+
+					<div class="encadrementAcc">
+						<img alt="Renault" src="imageweb/RenaultMeganeRS/sellerie1.jpg" height="140" width="150"> 
+
+						<div class="titre"> Sellerie cuir Recaro Alcantara </div> <div class="prix"> 2000€ </div>
+						<a href="imageweb/RenaultMeganeRS/sellerie1.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					<br>
+					<br>
+
+					<div class="encadrementAcc">
+						<img alt="Renault" src="imageweb/RenaultMeganeRS/sellerie2.jpg" height="140" width="150"> 
+
+						<div class="titre"> Sellerie cuir Alcantara</div> <div class="prix"> 1500€ </div>
+						<a href="imageweb/RenaultMeganeRS/sellerie2.jpg"> <div class="voirProduit"> Voir le produit </div> </a>
+					</div>
+
+					</div>
+
+
+
+				
+
+
+
+
+
+
+
+				</div>
+
+
+
+
+		</body>
+</html>
