0769223f49c5a3ba2a7a458d4528f0e22a2184eb
RyanLauret <66676905+RyanLauret@users.noreply.github.com>
2020-09-29 22:41:23 +0400

    Create tableau.php

---
+++ tableau.php
@@ -0,0 +1,34 @@
+        <h2>Contact</h2>
+
+<div class="container">
+  <form>
+  <div class="row">
+    <div class="col-25">
+      <label for="fname">Nom</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="fname" name="nom" placeholder="Votre nom..">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label for="lname">Prénom</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="lname" name="prenom" placeholder="Votre prénom..">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label for="subject">Sujet</label>
+    </div>
+    <div class="col-75">
+      <textarea id="Sujet" name="sujet" placeholder="Votre problème(s).." style="height:200px"></textarea>
+    </div>
+  </div>
+  <div class="row">
+    <input type="submit" value="Submit">
+  </div>
+  </form>
+</div>
+</html>
